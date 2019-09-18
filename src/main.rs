84ab92c22c38fc298f55a5d235bf237c2bbd7253
William Azevedo <willianray@gmail.com>
2019-09-18 08:15:33 -0300

    Pig latin feito.

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,28 @@ fn mode(numbers: &Vec<u32>) -> u32 {
         .expect("Cannot compute the mode of zero numbers")
 }
 
+fn is_vogal(string: &str) -> bool {
+    let vogals = ["a", "e", "i", "o", "u"];
+    vogals.iter().any(|&x| x == string)
+}
+
+fn pig_latin(string: &str) -> String {
+    match string.get(0..1).map(|first_letter| first_letter.to_lowercase()) {
+        None => String::from("string invalida"),
+        Some(first_letter) => {
+                if is_vogal(&first_letter) {
+                    format!("{}-{}", string, "hay")
+                } else {
+                    format!("{}{}-{}", &string[1..], first_letter, "ay")
+                }
+        }
+    }
+}
+
+fn to_uppercase(text: &str) -> String {
+    text.to_string()
+}
+
 fn main() {
     let mut integers = vec![1,2,3,4,5,6,7,8,9,10];
     let mut map_integers = HashMap::new();
@@ -31,5 +53,11 @@ fn main() {
     map_integers.insert("mode", mode(&integers) as f32);
 
     println!("{:#?}", map_integers);
+
+    println!("{}", pig_latin(""));
+    println!("{}", pig_latin("apple"));
+
+    let teste = String::from("teste");
+    println!("{}", to_uppercase(&teste));
 
 }
